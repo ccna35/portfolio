5bbe336b214f448fc858babc0bd6106d157fddab
Shawky Khalil <shawkykhalil@gmail.com>
2023-08-03 04:17:13 +0300

    added more sections

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello from the other side.");
+const title = document.querySelector(".mask__text");
+
+title.addEventListener("click", () => {
+  console.log("clicked");
+});
+
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   console.log("DOM fully loaded and parsed");
+// });
+
+// addEventListener("DOMContentLoaded", () => {
+//   title.classList.add("animateText");
+//   console.log("Loaded");
+// });
+
+// setTimeout(() => {
+//   title.classList.add("animateText");
+// }, 3000);
